49292be8d3da9e7cfb6d193e79e2dfa0cb713afe
user <thefurpineapple@gmail.com>
2018-11-26 14:04:36 +0200

    26112018_14_05

---
+++ TESTNUMPY3.py
@@ -0,0 +1,34 @@
+#f(x)=sin(x) Attēlotā funkcijā krustojuma punkts ir noverots,
+#f(x)=0, kad x=3,14159+pi*n, kur n=Z.
+
+from math import sin,fabs
+from time import sleep
+
+def f(x):
+    return sin(x)
+k=0
+# Definejam argumenta x robežas:
+a=1.1
+b=3.2
+# Apreķinam funkcijas vērtības dotajos punktos:
+funa=f(a)
+funb=f(b)
+# Pārbaudam, vai dotajā intervālā ir saknes:
+if(funa*funb>0.0):
+    print("Dotajā intervālā[%s,%s] sakņu nav"%(a,b))
+    sleep(1);exit()     #Ziņo uz ekrāna, gaida 1 sec. un darbu pabeidz
+else:
+    print("Dotajā intervālā sakne(s) ir!")
+# Definējam precizitāti, ar kādu meklēsim sakni:
+deltax=0.000000000000001
+# Sašaurinam saknes meklēšanas robežas:
+while(fabs(b-a)>deltax):
+    x=(a+b)/2;funx=f(x)
+    if (funa*funx<0.):
+        b=x
+        k=k+1
+    else:
+        a=x
+print("Sakne ir: ",x)
+print("f(x)=", sin(x))
+print("k=", k)
---
+++ dg17.py
@@ -0,0 +1,63 @@
+Python 3.6.5 (default, Apr  1 2018, 05:46:30) 
+[GCC 7.3.0] on linux
+Type "copyright", "credits" or "license()" for more information.
+>>> gnuplot
+Traceback (most recent call last):
+  File "<pyshell#0>", line 1, in <module>
+    gnuplot
+NameError: name 'gnuplot' is not defined
+>>> import sys
+>>> sys.path.append('/usr/local/anaconda3/lib/python3.6/site-packages')
+>>> gnuplot
+Traceback (most recent call last):
+  File "<pyshell#3>", line 1, in <module>
+    gnuplot
+NameError: name 'gnuplot' is not defined
+>>> 
+================== RESTART: /home/user/RTR105/TESTNUMPY3.py ==================
+Dotajā intervālā sakne(s) ir!
+Sakne ir:  3.1425781250000004
+>>> 
+================== RESTART: /home/user/RTR105/TESTNUMPY3.py ==================
+Dotajā intervālā sakne(s) ir!
+Sakne ir:  3.1425781250000004
+f(x)= -0.0009854712506998128
+k= 3
+>>> 
+================== RESTART: /home/user/RTR105/TESTNUMPY3.py ==================
+Dotajā intervālā sakne(s) ir!
+Sakne ir:  3.1425781250000004
+f(x)= -0.0009854712506998128
+k= 3
+>>> 
+================== RESTART: /home/user/RTR105/TESTNUMPY3.py ==================
+Dotajā intervālā sakne(s) ir!
+Sakne ir:  3.1425781250000004
+f(x)= -0.0009854712506998128
+k= 3
+
+>>> 
+================== RESTART: /home/user/RTR105/TESTNUMPY3.py ==================
+Dotajā intervālā sakne(s) ir!
+Sakne ir:  3.1416168212890634
+f(x)= -2.416769926779708e-05
+k= 7
+>>> 
+================== RESTART: /home/user/RTR105/TESTNUMPY3.py ==================
+Dotajā intervālā sakne(s) ir!
+Sakne ir:  3.1415927886962898
+f(x)= -1.351064965345797e-07
+k= 9
+>>> 
+================== RESTART: /home/user/RTR105/TESTNUMPY3.py ==================
+Dotajā intervālā sakne(s) ir!
+Sakne ir:  3.1416168212890634
+f(x)= -2.416769926779708e-05
+k= 7
+>>> 
+================== RESTART: /home/user/RTR105/TESTNUMPY3.py ==================
+Dotajā intervālā sakne(s) ir!
+Sakne ir:  3.141592653589793
+f(x)= 1.2246467991473532e-16
+k= 20
+>>> 
