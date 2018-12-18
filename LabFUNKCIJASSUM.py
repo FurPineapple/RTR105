59ea032572f04e8f1b91d7532072a0c9677f0fd0
user <thefurpineapple@gmail.com>
2018-12-18 18:59:52 +0200

    20181218_19_00

--- LabFUNKCIJASSUM.py
+++ LabFUNKCIJASSUM.py
@@ -3,17 +3,17 @@ import math
 
 def fasinh(x):
     k=0
-    a=0
+    a=(((-1)**0)*1*x**1)/((4**0)*(1**2)*1)
     S=a
     print("Izdruka no liet.f. a0=%6.2f S0=%6.2f"%(a,S))
 
     while k<500:
         k=k+1
-        R=()
-    a=0
-    S=S+a
-    if k==499:
-        print("Izdruka no liet.f.a%d=%6.2fS%d=%6.2f"%(k,a,k,S))
+        R=((-1**1)*(2*k-1)*(2*k)*(x**2))/((4**1)*(k**2)*2)
+        a=a*R
+        S=S+a
+        if k==499:
+            print("Izdruka no liet.f.a%d=%6.2fS%d=%6.2f"%(k,a,k,S))
     print("Izdruka no liet.f. a500=%6.2f S500=%6.2f"%(a,S))
     return S
 x=float(input("Lietotajs, ludzu, ievadi argmumentu(x):"))
@@ -27,7 +27,7 @@ print("                500                              ")
 print("             ____________                        ")
 print("             \            k                2k+1  ")
 print("              \       (-1)! * (2*k)!  * (x)      ")
-print("ahinh()%.2f)=  \  ____________________________   "%(x))
+print("ahinh(%.2f)=  \  ____________________________   "%(x))
 print("               /    k      2                     ")
 print("              /    4  * (k!) * (2*k+1)            ")
 print("             /___________                        ")
