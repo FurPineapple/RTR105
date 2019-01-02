1b120daaf413316c4528c86c8aa87f98e0e17d3a
FurPineapple <43132663+FurPineapple@users.noreply.github.com>
2019-01-02 20:27:30 +0200

    Add files via upload

--- TESTNUMPY3.py
+++ TESTNUMPY3.py
@@ -1,15 +1,15 @@
-#f(x)=sin(x) Attēlotā funkcijā krustojuma punkts ir noverots,
+#f(x)=arcsinh(x) Attēlotā funkcijā krustojuma punkts ir noverots,
 #f(x)=0, kad x=3,14159+pi*n, kur n=Z.
 
-from math import sin,fabs
+from math import asinh,fabs
 from time import sleep
 
 def f(x):
-    return sin(x)
+    return asinh(x)
 k=0
 # Definejam argumenta x robežas:
-a=1.1
-b=3.2
+a=-1
+b=2
 # Apreķinam funkcijas vērtības dotajos punktos:
 funa=f(a)
 funb=f(b)
@@ -30,5 +30,5 @@ while(fabs(b-a)>deltax):
     else:
         a=x
 print("Sakne ir: ",x)
-print("f(x)=", sin(x))
+print("f(x)=", asinh(x))
 print("k=", k)
